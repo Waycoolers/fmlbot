5bd85ddb87ae9656343ed910b017947b28832cc4
Waycoolers <mihazd01@mail.ru>
2025-11-10 04:29:51 +0300

    Add limit to random compliments

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -3,10 +3,13 @@ package bot
 import (
 	"context"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/Waycoolers/fmlbot/internal/config"
 	"github.com/Waycoolers/fmlbot/internal/storage"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 )
 
 type Bot struct {
@@ -25,6 +28,8 @@ func New(cfg *config.Config, store *storage.Storage) (*Bot, error) {
 
 func (b *Bot) Run() {
 	log.Printf("Бот %s запущен", b.api.Self.UserName)
+
+	_ = godotenv.Load()
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -48,13 +53,44 @@ func (b *Bot) Run() {
 		case "/compliment":
 			log.Print("Клиент вызвал: /compliment")
 			ctx := context.Background()
-			text, err := b.store.GetNextCompliment(ctx)
+
+			// преобразуем string значение из .env в int
+			limitStr := os.Getenv("LIMIT_COMPLIMENTS_PER_DAY")
+			dailyLimit, err := strconv.Atoi(limitStr)
+			if err != nil {
+				dailyLimit = 3 // дефолтное значение
+			}
+
+			userID := update.Message.Chat.ID
+
+			canSend, err := b.store.CanSendCompliment(ctx, userID, dailyLimit)
+			if err != nil {
+				log.Println(err)
+				break
+			}
+
+			if !canSend {
+				msg := tgbotapi.NewMessage(userID, "Комплименты на сегодня закончились.")
+				log.Printf("Бот ответил: %s", msg.Text)
+				_, err := b.api.Send(msg)
+				if err != nil {
+					log.Fatalf("Ошибка при отправке сообщения о том, что лимит комплиментов исчерпан: %v", err)
+				}
+				break
+			}
+
+			complimentID, text, err := b.store.GetNextCompliment(ctx)
 			if err != nil {
 				text = "😅 У меня сейчас нет комплиментов, но ты всё равно чудесная!"
 			}
-
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
 			log.Printf("Бот ответил: %s", msg.Text)
+
+			err = b.store.RecordCompliment(ctx, userID, complimentID)
+			if err != nil {
+				log.Fatalf("Ошибка при записи комплимента в таблицу с историей: %v", err)
+			}
+
 			_, err = b.api.Send(msg)
 			if err != nil {
 				log.Fatalf("Ошибка при отправке комплимента: %v", err)
--- internal/storage/compliments.go
+++ internal/storage/compliments.go
@@ -5,27 +5,51 @@ import (
 	"log"
 )
 
-func (s *Storage) GetNextCompliment(ctx context.Context) (string, error) {
-	var compliment string
+func (s *Storage) CanSendCompliment(ctx context.Context, userID int64, limit int) (bool, error) {
+	var count int
+	query := `
+	SELECT COUNT(*) 
+	FROM compliment_history
+	WHERE user_id = $1 AND created_at >= current_date
+	`
+	err := s.DB.QueryRow(ctx, query, userID).Scan(&count)
+	if err != nil {
+		return false, err
+	}
+	return count < limit, nil
+}
 
+func (s *Storage) RecordCompliment(ctx context.Context, userID int64, complimentID int) error {
+	query := `
+	INSERT INTO compliment_history (user_id, compliment_id)
+	VALUES ($1, $2)
+	`
+	_, err := s.DB.Exec(ctx, query, userID, complimentID)
+	return err
+}
+
+func (s *Storage) GetNextCompliment(ctx context.Context) (int, string, error) {
 	query := `
 	WITH next AS (
-		SELECT id, text
-		FROM compliments
-		WHERE count = (SELECT MIN(count) FROM compliments)
-		ORDER BY random()
-		LIMIT 1
+	    SELECT id, text
+	    FROM compliments
+	    WHERE count = (SELECT MIN(count) FROM compliments)
+	    ORDER BY random()
+	    LIMIT 1
 	)
 	UPDATE compliments c
 	SET count = count + 1
 	FROM next n
 	WHERE c.id = n.id
-	RETURNING n.text;
+	RETURNING n.id, n.text;
 	`
+	var id int
+	var text string
 
-	err := s.DB.QueryRow(ctx, query).Scan(&compliment)
+	err := s.DB.QueryRow(ctx, query).Scan(&id, &text)
 	if err != nil {
 		log.Fatalf("Ошибка при получении комплимента: %v", err)
 	}
-	return compliment, nil
+
+	return id, text, nil
 }
